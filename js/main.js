acaf752ac40d6702de11e72d41bf0d81368fd1e8
Lukasz Trzaska <lukasztrzaska81@gmail.com>
2018-04-23 22:13:06 +0200

    json

--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@
         var pattern = /^[a-ząśżźćęółń]+$/i;
         var firstName = pattern.test(input.val());
         if(firstName){
-            $('#firstname').css({"border":"1px solid green"});
+            $('#firstname').css({"border":"1px solid green"}).addClass("ok");
             input.next('.info').text("");
+
         }
         else{
             input.next('.info').text("Wprowadź poprawne imię").css({"color":"red"});
-            $('#firstname').css({"border":"1px solid #ccc"});
+            $('#firstname').css({"border":"1px solid #ccc"}).removeClass('ok');
         }
     });
 
@@ -20,12 +21,12 @@
         var pattern = /^[a-ząśżźćęółń]+(?:\s*-\s*)?[a-ząśżźćęółń]+$/i;
         var lastName = pattern.test(input.val());
         if(lastName){
-            $('#lastname').css({"border":"1px solid green"});
+            $('#lastname').css({"border":"1px solid green"}).addClass("ok");
             input.next('.info').text("");
         }
         else{
             input.next('.info').text("Wprowadź poprawne nazwisko").css({"color":"red"});
-            $('#lastname').css({"border":"1px solid #ccc"});
+            $('#lastname').css({"border":"1px solid #ccc"}).removeClass('ok');
         }
     });
 
@@ -34,30 +35,30 @@
         var pattern = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
         var email = pattern.test(input.val());
         if(email){
-            $('#email').css({"border":"1px solid green"});
+            $('#email').css({"border":"1px solid green"}).addClass("ok");
             input.next('.info').text("");
         }
         else{
             input.next('.info').text("Wprowadź poprawny adres email").css({"color":"red"});
-            $('#email').css({"border":"1px solid #ccc"});
+            $('#email').css({"border":"1px solid #ccc"}).removeClass('ok');
         }
     });
 
-      $('#phone').on('blur', function() {
+    $('#phone').on('blur', function() {
         var input = $(this);
         var pattern = /^(?:\(?\+?48)?(?:[-\.\(\)\s]*(\d)){9}\)?$/;
         var phone = pattern.test(input.val());
         if(phone){
-            $('#phone').css({"border":"1px solid green"});
+            $('#phone').css({"border":"1px solid green"}).addClass("ok");
             input.next('.info').text("");
         }
         else{
             input.next('.info').text("Wprowadź poprawny numer telefonu").css({"color":"red"});
-            $('#phone').css({"border":"1px solid #ccc"});
+            $('#phone').css({"border":"1px solid #ccc"}).removeClass('ok');
         }
     });
 
-       $('#postalcode').on('blur', function() {
+    $('#postalcode').on('blur', function() {
         var input = $(this);
         var pattern = /^[0-9]{2}-[0-9]{3}/;
         var postalcode = pattern.test(input.val());
@@ -70,6 +71,32 @@
             $('#postalcode').css({"border":"1px solid #ccc"});
         }
     });
+
+
+    $('.contact100-form-btn').click(function(event){
+        event.preventDefault();
+        var name = $('#firstname');
+        var lastname = $('#lastname');
+        var email = $('#email');
+        var phone = $('#phone');
+            
+        if(name.hasClass('ok') && lastname.hasClass('ok') && email.hasClass('ok') && phone.hasClass('ok')) {
+            $.ajax({ 
+                type: 'GET', 
+                url:'response.json', 
+                data: { get_param: 'value' }, 
+                dataType: 'json',
+                success: function (data) { 
+                    console.log(data);
+                     $('.success').html(data.status.message);
+                }
+            });
+        }
+        else {  
+              alert("Uzupełnij wymagane pola!"); 
+        }
+    });
+
 
 
     /*==================================================================
